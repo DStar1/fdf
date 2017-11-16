a190f221950692c54d98cc0e662ff818d38cede6
Harrison Smith <hasmith@e1z2r9p30.42.us.org>
2017-11-15 20:06:37 -0800

    added full line drawing

--- main.c
+++ main.c
@@ -43,25 +43,25 @@ int		*znc(t_points **stmp, char **arr, int *len, int y)
 	return (0);
 }
 
-void printpoints(int xlen, int ylen, t_points **twodstruct)
-{
-	int y = 0;
-	int x = 0;
-	while (y < ylen)
-	{
-		x = 0;
-		while (x < xlen)
-		{
-			//printf("c: %s, z: %d | ", twodstruct[y][x].c, twodstruct[y][x].z); //trying to make a 2d struct array
-			printf("(%d, %d)z=%d||", twodstruct[y][x].y, twodstruct[y][x].x, twodstruct[y][x].z);
-			x++;
-		}
-		printf("\n");
-		free(twodstruct[y]);
-		y++;
-	}
-	free(twodstruct);
-}
+// void printpoints(int xlen, int ylen, t_points **twodstruct)
+// {
+// 	int y = 0;
+// 	int x = 0;
+// 	while (y < ylen)
+// 	{
+// 		x = 0;
+// 		while (x < xlen)
+// 		{
+// 			//printf("c: %s, z: %d | ", twodstruct[y][x].c, twodstruct[y][x].z); //trying to make a 2d struct array
+// 			printf("(%d, %d)z=%d||", twodstruct[y][x].y, twodstruct[y][x].x, twodstruct[y][x].z);
+// 			x++;
+// 		}
+// 		printf("\n");
+// 		free(twodstruct[y]);
+// 		y++;
+// 	}
+// 	free(twodstruct);
+// }
 
 t_points **points(char *filename, t_master *master){
 	int		fd;
@@ -107,90 +107,83 @@ t_points **points(char *filename, t_master *master){
 void draw(t_master *master, t_points **coords, int y11, int x11, int y22, int x22)
 {
 ////////////////////writes line to the screen//////////////////
-	int x1 = coords[y11][x11].x;
-	int y1 = coords[y11][x11].y;
-	int x2 = coords[y22][x22].x;
-	int y2 = coords[y22][x22].y;
-	printf("(%d, %d) || (%d, %d)\n", y1, x1, y2, x2);
-    int dx = x2 - x1;
-    int dy = y2 - x1;
-				//printf("%d,%d,%d,%d\n", y, x, y, x + 100);
-	int p;
-	int x = x1;
-	int y = y1;
-	if (y1 == y2) //straight horizontal line
+	double x1 = coords[y11][x11].x;
+	double y1 = coords[y11][x11].y;
+	double x2 = coords[y22][x22].x;
+	double y2 = coords[y22][x22].y;
+
+	// int x1 = 70;
+	// int y1 = 90;
+	// int x2 = 70;
+	// int y2 = 0;
+	
+	int numerator;
+	int longest;
+	int shortest;
+	int i;
+	int w;
+	int h;
+	i = 0;
+	w = x2 - x1;
+    h = y2 - y1;
+    double dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+    if (w<0) dx1 = -1 ; else if (w>0) dx1 = 1;
+    if (h<0) dy1 = -1 ; else if (h>0) dy1 = 1;
+    if (w<0) dx2 = -1 ; else if (w>0) dx2 = 1;
+    longest = abs(w) ; //possibly create ft_abs function
+    shortest = abs(h) ;
+    if (!(longest>shortest))
 	{
-		while (x < x2)
-		{
-			mlx_pixel_put(master->mlx, master->win, x, y, 0x000FFFFF);
-			x++;
-		}
-	}
-	else if (x1 == x2) //straight vertical line
+        longest = abs(h) ;
+        shortest = abs(w) ;
+        if (h<0) dy2 = -1 ; else if (h>0) dy2 = 1 ;
+        dx2 = 0 ;
+    }
+    numerator = longest >> 1;
+    while (i<=longest)
 	{
-		while (y < y2)
+        mlx_pixel_put(master->mlx, master->win, x1, y1, 0x000FFFFF);
+        numerator += shortest;
+        if (!(numerator<longest))
 		{
-			mlx_pixel_put(master->mlx, master->win, x, y, 0x000FFFFF);
-			y++;
-		}
-	}
-	else if (dx>=dy){
-		p = 2*dy-dx;
-		while (x<x2)
-		{
-			if (p >= 0)
-			{
-				mlx_pixel_put(master->mlx, master->win, x, y, 0x000FFFFF);
-				y++;
-				p = p + 2 * dy - 2 * dx;
-			}
-			else
-			{
-				mlx_pixel_put(master->mlx, master->win, x, y, 0x000FFFFF);
-				p = p + 2 * dy;
-			}
-			x++;
-		}
-	}
-	else if (dy >= dx) 
-	{
-		p = 2*dx-dy;
-		while (y<y2)
-		{
-			if (p >= 0)
-			{
-				mlx_pixel_put(master->mlx, master->win, x, y, 0x000FFFFF);
-				x++;
-				p = p + 2 * dx - 2 * dy;
-			}
-			else
-			{
-				mlx_pixel_put(master->mlx, master->win, x, y, 0x000FFFFF);
-				p = p + 2 * dx;
-			}
-			y++;
-		}
-	}
+            numerator -= longest;
+            x1 += dx1;
+            y1 += dy1;
+        }
+		else {
+            x1 += dx2;
+            y1 += dy2;
+        }
+		i++;
+    }
 	return ;
 }
 
 
 //dont fully understand how to impliment
-void	rotate(t_points ***coords, int ang, int x, int y)
+t_points	rotate(t_points coords, int ang, int x, int y)
 {
 	//z
-	int x1 = coords[y][x]->x;
-	int y1 = coords[y][x]->y;
-	coords[y][x]->x = x1 * cos(ang) - y1 * sin(ang);
-	coords[y][x]->y = x1 * sin(ang) + y1 * cos(ang);
-	// coords[y][x]->x = (cos(ang) - sin(ang))*coords[y][x]->x;
-	// coords[y][x]->y = (sin(ang) + cos(ang)) * coords[y][x]->y;
+	t_points coords1;
+	//printf("%d", (int)coords.x);
+
+	ang = ang * (M_PI/180);
+ 	coords1.x = (x + 1 * cos(ang)) - (y + 1 * sin(ang));
+ 	coords1.y = (x + 1 * sin(ang)) + (y + 1 * cos(ang));
 
 	//y
 	//coords[y][x]->x = coords[y][x]->x * cos(ang) + coords[y][x]->z * sin(ang);
-
+	return (coords1);
 
 } //http://graphics.cs.cmu.edu/nsp/course/15-462/Spring04/slides/04-transform.pdf
+
+// void	rotate(t_points ***coords, int ang, int x, int y)
+// {
+// 	ang = ang * (M_PI/180);
+// 	coords[y][x]->x = (x+1 * cos(ang)) - (y+1 * sin(ang));
+// 	coords[y][x]->y = (x+1 * sin(ang)) + (y+1 * cos(ang));
+// 	return ;
+// }
 
 
 int main(int ac, char **av)
@@ -202,29 +195,36 @@ int main(int ac, char **av)
 	
 	if (ac == 2)
 	{
-		master.wsize = 400;
+		master.wsize = 1000;
 		master.xlen = 0;
 		master.ylen = 0;
 		coords = points(av[1], &master); //added pointer
 		master.mlx = mlx_init();
 		master.win = mlx_new_window(master.mlx, master.wsize, master.wsize, av[1]);
 		//x = 0;
+		int ang = 45 * (M_PI/180);
+		//ang = 0;
 		y = 0;
 		while(y < master.ylen)
 		{
 			x = 0;
 			while (x < (master.xlen))
 			{
-				coords[y][x].x = (x + 1) * 20 + 70;//(master.wsize / (master.xlen + 1));
-				coords[y][x].y = (y + 1) * 20 + 70;//(master.wsize / (master.ylen + 1));
-				//printf("((%d, %d):", coords[y][x].y, coords[y][x].x);
-				//rotate(&coords, 45, x, y);
-				//printf("(%d, %d))|", coords[y][x].y, coords[y][x].x);
+
+				// printf("((%d, %d):", coords[y][x].y, coords[y][x].x);
+				//printf("x:%d, y%d: angle: %f,\n", x, y, (x * cos(ang)) - (y * sin(ang)));
+				//coords[y][x] = rotate(coords[y][x], 0, x, y);
+				coords[y][x].x = ((coords[y][x].x + 1) * cos(ang)) - ((coords[y][x].y + 1) * sin(ang)); //was just x or y in place of coords[y][x].x/y
+ 				coords[y][x].y = ((coords[y][x].x + 1) * sin(ang)) + ((coords[y][x].y + 1) * cos(ang));
+				
+				coords[y][x].x = (coords[y][x].x) * (master.wsize / (master.xlen + 1))/2+500;
+				coords[y][x].y = (coords[y][x].y) * (master.wsize / (master.ylen + 1))/2+100;
+				//printf("(%d, %d))|", (int)coords[y][x].y, (int)coords[y][x].x);
 				x++;
 			}
 			y++;
 		}
-				printf("%d, %d\n", master.ylen, master.xlen);
+				// printf("%d, %d\n", master.ylen, master.xlen);
 		//		printf("(%d, %d) |\n", coords[1][1].y, coords[1][1].x);
 		y = 0;
 		while(y < master.ylen)
@@ -233,16 +233,28 @@ int main(int ac, char **av)
 			while (x < master.xlen)
 			{
 				//printf("(%d, %d) |\n", coords[y][x].y, coords[y][x].x);
-				if (x < (master.xlen)-1)
-				{
+				//printf("(%d, %d)\n", (int)coords[y][x].y, (int)coords[y][x].x);
+				// printf("(%d, %d)\n\n", (int)coords[y+1][x+1].y, (int)coords[y+1][x+1].x);
+				//draw(&master, coords, y, x, y + 1, x + 1);
+				// if (x < (master.xlen)-1 && y < (master.ylen)-1)
+				// {
+				 	// draw(&master, coords, y, x, y + 1, x + 1);
+				// }
+				if (x < (master.xlen)-1){
 					draw(&master, coords, y, x, y, x + 1);
-				}
-				if (y < (master.ylen)-1)
-					draw(&master, coords, y, x, y+1, x);
+					printf("p1(%d, %d) p2(%d, %d)\n", (int)coords[y][x].y, (int)coords[y][x].x, (int)coords[y][x+1].y, (int)coords[y][x+1].x);
+					}
+				if (y < (master.ylen)-1){
+					draw(&master, coords, y, x, y + 1, x);
+					printf("p1(%d, %d) p2(%d, %d)\n\n", (int)coords[y][x].y, (int)coords[y][x].x, (int)coords[y+1][x].y, (int)coords[y+1][x].x);
+					//printf("(%d, %d)\n\n", (int)coords[y][x].y, (int)coords[y+1][x].x);
+					}
+				
 				x++;
 			}
 			y++;
 		}
+		//draw(&master, coords, y, x, y+1, x);
 		ft_putchar('\n');
 		// printf("x%d, xend %d\n", x, master.wsize / (master.xlen + 1) * master.xlen);
 		// printf("y%d, yend%d\n", y, master.wsize / (master.ylen + 1) * master.ylen);
